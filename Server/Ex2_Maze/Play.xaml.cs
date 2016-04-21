a913fcb3b750528dba1bbf56a9d38a0b06f66c37
NAVAJAVA <nshemoul@gmail.com>
2016-04-21 16:33:33 +0300

    my thing

--- Server/Ex2_Maze/Play.xaml.cs
+++ Server/Ex2_Maze/Play.xaml.cs
@@ -12,21 +12,75 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.ComponentModel;
+using Ex1_Maze;
 
 namespace Ex2_Maze
 {
     /// <summary>
     /// Interaction logic for Play.xaml
     /// </summary>
-    public partial class Play : Window
+    public partial class Play : Window, INotifyPropertyChanged
     {
         private string gen = "1 Maze_ ";
         private Random rand;
         MainWindow mainWin;
+        public string _Image { get; set; }
 
-        public Play(MainWindow mw)
+        private void ShowImage(object sender, KeyEventArgs e)
         {
-            this.mainWin = mw;
+            if (e.Key == Key.Down)
+            {
+                this.Title = "You pressed dowm!";
+                //change the gui that the man is in one cube down
+            }
+
+            if (e.Key == Key.Right)
+            {
+                this.Title = "You pressed right!";
+                //change the gui that the man is in one cube right
+            }
+
+            if (e.Key == Key.Up)
+            {
+                this.Title = "You pressed up!";
+                //change the gui that the man is in one cube up
+            }
+
+            if (e.Key == Key.Left)
+            {
+                this.Title = "You pressed left!";
+                //change the gui that the man is in one cube left
+            }
+        }
+
+        public string Image
+        {
+            get
+            {
+                return _Image;
+            }
+
+            set
+            {
+                _Image = value;
+                NotifyPropertyChanged("Image");
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+        public Play(ViewModel vm)
+        {
+            this.DataContext = vm;
             WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
             InitializeComponent();
             
