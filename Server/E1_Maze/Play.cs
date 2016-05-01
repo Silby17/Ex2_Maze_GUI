9d3d7e0df5ee0e08740ae282be198658c9db29ef
Yossi Silberhaft <yossisilb@gmail.com>
2016-05-01 14:08:04 +0300

    Added new Play class

---
+++ Server/E1_Maze/Play.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E1_Maze
+{
+    public class Play
+    {
+        public string Name { get; set; }
+        public string Move { get; set; }
+
+        public Play() { }
+    }
+}
