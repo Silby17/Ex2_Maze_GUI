284d49f78cec443446dd8dffa2273770c7558027
Yossi Silberhaft <yossisilb@gmail.com>
2016-05-01 16:10:41 +0300

    Pay working

--- Server/E1_Maze/Play.cs
+++ Server/E1_Maze/Play.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace E1_Maze
+
+namespace Ex1_Maze
 {
     public class Play
     {
