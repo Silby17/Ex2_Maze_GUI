4e8ca5480ff98a3d6a92c9a5f9e4bba01f90b1c0
Yossi Silberhaft <yossisilb@gmail.com>
2016-05-02 11:55:38 +0300

    Final Commit - Focus Set

--- Server/Ex2_Maze/Multiplayer.xaml.cs
+++ Server/Ex2_Maze/Multiplayer.xaml.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 using System.Windows;
 using System;
-
+using System.Media;
 
 namespace Ex2_Maze
 {
@@ -14,8 +14,10 @@ namespace Ex2_Maze
     {
         ViewModel viewModel;
         private bool gameInMotion;
-        public string MazeName;  
-             
+        public string MazeName;
+        public string GameName;
+        private SoundPlayer MusicPlayer;
+
 
         /// <summary>
         /// Constructor Method that receives the ViewModel</summary>
@@ -24,13 +26,32 @@ namespace Ex2_Maze
         {
             this.viewModel = vm;
             this.DataContext = vm;
-            this.viewModel.PropertyChanged += ReceiveEvent ;
+            //Subscribes to events 
+            this.viewModel.PropertyChanged += ReceiveEvent;
+            Closing += viewModel.OnWindowClosing;
             WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
             InitializeComponent();
-            //SoundPlayer MusicPlayer = new System.Media.SoundPlayer(@"C:\Users\Nava\Source\Repos\Ex2_Maze_GUI\Server\Ex2_Maze\sovtoda.wav");
-            //MusicPlayer.Play();
+            StartMusic();
         }
-       
+
+        
+        /// <summary>
+        /// This method will start the Background Music for the game</summary>
+        public void StartMusic()
+        {
+            string fileName = "Shuv Toda.wav";
+            string path = System.IO.Path.Combine(Environment.CurrentDirectory, fileName);
+            this.MusicPlayer = new SoundPlayer(path);
+            MusicPlayer.Play();
+        }
+
+
+        /// <summary>
+        /// Stops the music playing
+        /// </summary>
+        public void StopMusic()
+        { this.MusicPlayer.Stop(); }
+
 
         /// <summary>
         /// This is the OnClick handler for the Start button</summary>
@@ -41,34 +62,37 @@ namespace Ex2_Maze
             //Checks if there is a connection to the server
             if (viewModel.VM_Connected == false)
             {
+                //No connection - Show message
                 MessageBoxImage icon = MessageBoxImage.Error;
                 MessageBox.Show("No Connection with Server", "Connection Error",
                     MessageBoxButton.OK, icon);
             }
+            //This is Connection
             else
             {
+                //Show Confirmation Message
                 MessageBoxResult result = MessageBox.Show("Do you want to start a new game?",
                     "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
                 if (result == MessageBoxResult.No)
                 {
                     CloseWindow();
                 }
-                //TODO to reload the window
+                //User wants to start new Game
                 else if (result == MessageBoxResult.Yes)
                 {
                     if(gameInMotion == true)
                     {
                         CloseWindow();
                     }
+                    //Checks if the user has entered a Game Name
                     else if (txtbMaze.Text == "")
-                    {
-                        MessageBox.Show("Please enter in a Game Name");
-                    }
+                    { MessageBox.Show("Please enter in a Game Name");}
                     else
                     {
-                        string mazeName = txtbMaze.Text;
+                        string name = txtbMaze.Text;
+                        GameName = txtbMaze.Text;
                         string gen = "3 ";
-                        gen += mazeName;
+                        gen += name;
                         //Shows the progress indicator
                         ProgIndicator.IsBusy = true;
                         //Disables the input box
@@ -82,9 +106,13 @@ namespace Ex2_Maze
                              ProgIndicator.IsBusy = false;
                              viewModel.model.StartThread();
                              InitializeComponent();
+                             //Sets the sources for the Data Binding
                              myMaze.ItemsSource = viewModel.VM_Maze;
                              plr2.ItemsSource = viewModel.VM_Player2Maze;
+                             //Sets the focus on the Players Maze displayed
+                             myMaze.Focus();
                              gameInMotion = true;
+                             this.MazeName = viewModel.VW_MazeName;
                          }, TaskScheduler.FromCurrentSynchronizationContext()
             );
                     }
@@ -102,9 +130,7 @@ namespace Ex2_Maze
             MessageBoxResult result = MessageBox.Show("Do you want to close this window?",
                 "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
             if (result == MessageBoxResult.Yes)
-            {
-                CloseWindow();
-            }
+            {CloseWindow();}
         }
 
 
@@ -114,15 +140,26 @@ namespace Ex2_Maze
         /// <param name="e"></param>
         private void Suggestion_Click(object sender, RoutedEventArgs e)
         {
+            //Checks for Connection with the server
             if (viewModel.VM_Connected == false)
             {
                 MessageBoxImage icon = MessageBoxImage.Error;
                 MessageBox.Show("No Connection with Server", "Connection Error",
                     MessageBoxButton.OK, icon);
             }
+            else if(!gameInMotion)
+            {
+                MessageBoxImage icon = MessageBoxImage.Error;
+                MessageBox.Show("No Game in Progress", "Game error",
+                    MessageBoxButton.OK, icon);
+            }
             else
             {
-
+                //Send Request to server to get solved maze
+                Random rand = new Random();
+                int solveType = rand.Next(0, 2);
+                string solve = "2 " + MazeName + " " + solveType.ToString();
+                viewModel.Command(solve);
             }
         }
 
@@ -174,11 +211,17 @@ namespace Ex2_Maze
         /// multiplayer window needs to be closed</summary>
         public void CloseWindow()
         {
+            MusicPlayer.Stop();
             viewModel.model.KillThread();
-            this.Dispatcher.Invoke((Action)(() =>
+            if(gameInMotion)
             {
-                this.Close();
-            }));
+                this.Dispatcher.Invoke((Action)(() =>
+                {
+                    this.Close();
+                }));
+            }
+            else
+            {this.Close();}
         }
 
 
@@ -191,20 +234,24 @@ namespace Ex2_Maze
             if (e.PropertyName.Equals("VM_Winner"))
             {
                 MessageBoxImage icon = MessageBoxImage.Information;
-                MessageBox.Show("You have reached the end!", "You Won", MessageBoxButton.OK, icon);
+                MessageBox.Show("You have reached the end!",
+                    "You Won", MessageBoxButton.OK, icon);
+                MusicPlayer.Stop();
                 CloseWindow();
             }
+            //The Second Player wins
             else if(e.PropertyName.Equals("VM_P2_Winner"))
             {
                 MessageBoxImage icon = MessageBoxImage.Information;
-                MessageBox.Show("Player 2 beat you to the end", "You Lose", MessageBoxButton.OK, icon);
+                MessageBox.Show("Player 2 beat you to the end",
+                    "You Lose", MessageBoxButton.OK, icon);
+                MusicPlayer.Stop();
                 CloseWindow();
             }
-            else if (e.PropertyName.Equals("VM_Player_Moved"))
-            {
-                Refresh();
-            }
-
+            //The second Player Moved
+            else if (e.PropertyName.Equals("VM_Player_Moved") ||
+                (e.PropertyName.Equals("VM_SuggestionReceived")))
+            {Refresh();}
         }
     }
 }
