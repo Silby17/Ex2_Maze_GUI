1172f758c7dffa7f6eadd1aaebfe3f29384203ba
Yossi Silberhaft <yossisilb@gmail.com>
2016-05-02 12:04:32 +0300

    Added Start music Method

--- Server/Ex2_Maze/Play.xaml.cs
+++ Server/Ex2_Maze/Play.xaml.cs
@@ -1,20 +1,8 @@
-﻿using Ex1_Maze;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Media;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.ComponentModel;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using System.Web.Script.Serialization;
-using System.ComponentModel;
+using System.Windows;
+using System.Media;
+using System;
 
 namespace Ex2_Maze
 {
@@ -23,33 +11,46 @@ namespace Ex2_Maze
     /// </summary>
     public partial class Play : Window
     {
+        private SoundPlayer MusicPlayer;
+        private string MazeName;
         private Random rand;
         ViewModel viewModel;
-        private string MazeName;
-        private int WIDTH;
-        private int HEIGHT;
-        private SoundPlayer MusicPlayer;
-
+        private bool GameStarted;
+        
+        /// <summary>
+        /// Constructor Method</summary>
+        /// <param name="vm">ViewModel</param>
         public Play(ViewModel vm)
         {
-            this.WIDTH  = Int32.Parse(System.Configuration.ConfigurationManager.AppSettings["Width"]);
-            this.HEIGHT = Int32.Parse(System.Configuration.ConfigurationManager.AppSettings["Bredth"]);
             this.viewModel = vm;
             this.DataContext = vm;
-            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+            //Subscribe the viewModel to Closing events of this window
+            Closing += viewModel.OnWindowClosing;
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
             InitializeComponent();
             this.MazeName = "";
             this.viewModel.PropertyChanged += delegate (object seder, PropertyChangedEventArgs e)
             {
                 ReceiveEvent(e.PropertyName);
             };
-            /**
+            StartMusic();
+        }
+
+
+        /// <summary>
+        /// Starts background music</summary>
+        public void StartMusic()
+        {
             string fileName = "Shuv Toda.wav";
             string path = System.IO.Path.Combine(Environment.CurrentDirectory, fileName);
             this.MusicPlayer = new SoundPlayer(path);
             MusicPlayer.Play();
-            **/
         }
+
+        /// <summary>
+        /// This method will stop the music Player</summary>
+        public void StopMusic()
+        { this.MusicPlayer.Stop(); }
 
 
         /// <summary>
@@ -61,7 +62,8 @@ namespace Ex2_Maze
             if(eventData == "VM_Winner")
             {
                 MessageBoxImage icon = MessageBoxImage.Information;
-                MessageBox.Show("You have reached the end!", "You Won", MessageBoxButton.OK, icon);
+                MessageBox.Show("You have reached the end!", "You Won",
+                    MessageBoxButton.OK, icon);
                 this.Close();
             }
         }
@@ -82,26 +84,29 @@ namespace Ex2_Maze
             }
             else
             {
+                //Show confirmation Message
                 MessageBoxResult result = MessageBox.Show("Do you want to start a new game?",
                     "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
                 if (result == MessageBoxResult.No)
                 {
                     //Start game from the begining
                 }
-                //TODO to reload the window
                 else if (result == MessageBoxResult.Yes)
                 {
                     String gen = "1 Maze";
                     this.rand = new Random();
                     string num = (rand.Next(0, 30)).ToString();
                     gen += num += " ";
+                    //Chose a random Algo to Generate Maze
                     int type = rand.Next(0, 2);
                     gen += type.ToString();
-                    this.MazeName = gen;
+                    //Send Generate Command to the server
                     viewModel.Command(gen);
                     InitializeComponent();
                     this.MazeName = viewModel.VW_MazeName;
+                    //Set Source for data binding
                     lst.ItemsSource = viewModel.VM_Maze;
+                    GameStarted = true;
                 }
             }
         }
@@ -128,29 +133,30 @@ namespace Ex2_Maze
         /// <param name="e"></param>
         private void Suggestion_Click(object sender, RoutedEventArgs e)
         {
+            //Checks if there is a connection to the server
             if (viewModel.VM_Connected == false)
             {
                 MessageBoxImage icon = MessageBoxImage.Error;
                 MessageBox.Show("No Connection with Server", "Connection Error",
                     MessageBoxButton.OK, icon);
             }
-            else if(this.MazeName == "")
+            //Check if there is a game in progress
+            else if(!GameStarted)
             {
                 MessageBoxImage icon = MessageBoxImage.Error;
-                MessageBox.Show("No Maze Loaded", "Maze Error",
+                MessageBox.Show("No Game started", "Game error",
                     MessageBoxButton.OK, icon);
             }
             else
             {
+                //Send Solve Request to the Server
                 int solveType = rand.Next(0, 2);
                 string solve = "2 " + this.MazeName + " " + solveType.ToString();
                 viewModel.Command(solve);
                 lst.Items.Refresh();
             }
         }
-
-
-
+        
 
         /// <summary>
         /// This will handle any keys pressed on the play window
